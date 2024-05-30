8efccb3e68bf53fa201b249a838b8c832d53fd6e
dmystical-coder <dmystical.coder@gmail.com>
2024-05-30 20:01:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/" width="125" height="125" /> -->
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Welcome to the Homepage</h1>
+  </main>
+</template>
